af115281c6824a1e2cb55bae706fb41a3333e3b6
tytaeyoun <londonty5847@gmail.com>
2023-07-24 16:06:40 +0900

    Add a Greeter function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./styles.css";
 
+function Greeter() {
+    return <h1> Hello! This is a Greeter </h1>
+  }
+
 export default function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
       <h2>Start editing to see some magic happen!</h2>
       <p>hello what's up how u doing</p>
+      <Greeter/>
     </div>
   );
 }
